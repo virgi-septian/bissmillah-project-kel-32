df0b9a110a7972ea77f0a481c95db5b7bba239e6
virgi-septian <virgi-septian77@gmail.com>
2022-07-28 14:59:25 +0700

    (backend) menambah controller, model, dan migration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Http\Controllers\KategoriProdukController;
-use App\Http\Controllers\NilaiController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\SiswaController;
-use App\Http\Controllers\WaliController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -35,12 +31,4 @@ Route::get('/hallo', function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('post', PostController::class);
-
-Route::resource('siswa', SiswaController::class);
-
-Route::resource('nilai', NilaiController::class);
-
 Route::resource('kategori_produk', KategoriProdukController::class);
-
-Route::resource('wali', WaliController::class);
